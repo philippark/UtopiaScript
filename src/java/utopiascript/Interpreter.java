8239e4f72fce4a4c816d517f1cac8048a9fcca62
philippark <philippark271@gmail.com>
2025-09-30 16:38:53 -0400

    add environment and support for variables

--- src/java/utopiascript/Interpreter.java
+++ src/java/utopiascript/Interpreter.java
@@ -3,6 +3,8 @@ package utopiascript;
 import java.util.List;
 
 class Interpreter implements Expr.Visitor<Object>, Stmt.Visitor<Void> {
+    private Environment environment = new Environment();
+
     void interpret(List<Stmt> statements){
         try {
             for (Stmt statement : statements){
@@ -158,5 +160,22 @@ class Interpreter implements Expr.Visitor<Object>, Stmt.Visitor<Void> {
         if (a == null && b == null) return true;
         if (a == null) return false;
         return a.equals(b);
+    }
+
+    @Override 
+    public Object visitVariableExpr(Expr.Variable expr) {
+        return environment.get(expr.name);
+    }
+
+    @Override
+    public Void visitVarStmt(Stmt.Var stmt) {
+        Object value = null;
+
+        if (stmt.initializer != null) {
+            value = evaluate(stmt.initializer);
+        }
+
+        environment.define(stmt.name.lexeme, value);
+        return null;
     }
 }
