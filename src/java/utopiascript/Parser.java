fb5d5c6a4181e7ae4454d654846de336a66a5973
philippark <philippark271@gmail.com>
2025-10-21 16:42:41 -0400

    implement for loops

--- src/java/utopiascript/Parser.java
+++ src/java/utopiascript/Parser.java
@@ -2,6 +2,7 @@ package utopiascript;
 
 import static utopiascript.TokenType.*;
 
+import java.util.Arrays;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -93,6 +94,7 @@ public class Parser {
         if (match(LEFT_BRACE)) return new Stmt.Block(block());
         if (match(SE)) return ifStatement();
         if (match(DUM)) return whileStatement();
+        if (match(POR)) return forStatement();
 
         return expressionStatement();
     }
@@ -142,6 +144,58 @@ public class Parser {
         Stmt body = statement();
 
         return new Stmt.While(condition, body);
+    }
+
+    /**
+     * @brief Parses the for statement, which is composed of: 
+     * for (initializer; condition; increment). 
+     * The initializer, condition, and increment are optional.
+     * The for loop is syntatic sugar that is desugarized to rely on the while loop.
+     * @return A parse tree for the for statement
+     */
+    private Stmt forStatement() {
+        consume(LEFT_PAREN, "Expect '(' after 'for'.");
+
+        Stmt initializer;
+        if (match(SEMICOLON)) {
+            initializer = null;
+        } else if (match(VAR)) {
+            initializer = varDeclaration();
+        } else {
+            initializer = expressionStatement();
+        }
+
+        Expr condition = null;
+        if (!check(SEMICOLON)) {
+            condition = expression();
+        }
+        consume(SEMICOLON, "Expect ';' after loop condition");
+
+        Expr increment = null;
+        if (!check(RIGHT_PAREN)) {
+            increment = expression();
+        }
+        consume(RIGHT_PAREN, "Expect ')' after for clauses.");
+
+        Stmt body = statement();
+
+        if (increment != null) {
+            body = new Stmt.Block(
+                Arrays.asList(
+                    body,
+                    new Stmt.Expression(increment)
+                )
+            );
+        }
+
+        if (condition == null) condition = new Expr.Literal(true);
+        body = new Stmt.While(condition, body);
+
+        if (initializer != null) {
+            body = new Stmt.Block(Arrays.asList(initializer, body));
+        }
+
+        return body;
     }
 
     private Stmt varDeclaration() {
